a1f25af3462125fe7858a0271c3f6f2ceba841c7
Hisoka <hyosoka187@gmail.com>
2015-05-09 18:26:12 +0700

    Third Commit....

--- src/com/hisoka/add_remove_view_prog/AddRemoveMainActivity.java
+++ src/com/hisoka/add_remove_view_prog/AddRemoveMainActivity.java
@@ -2,12 +2,14 @@ package com.hisoka.add_remove_view_prog;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TableRow;
@@ -40,36 +42,52 @@ public class AddRemoveMainActivity extends Activity {
 		
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,spinnerContent);
 		
+		final TableRow row0 = new TableRow(this);
 		final TableRow row1 = new TableRow(this);
 		final TableRow row2 = new TableRow(this);
 		final TableRow row3 = new TableRow(this);
 		
+		TextView garis = new TextView(this);
 		Spinner spin = new Spinner(this);
 		TextView jenis = new TextView(this);
 		TextView keterangan = new TextView(this);
+		EditText ket = new EditText(this);
 		TextView total = new TextView(this);
-		
+		EditText tot = new EditText(this);
 		
 		TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT);
-		params.setMargins(78, 13, 97, 29);
+		params.setMargins(78, 7, 97, 29);
 		
+		TableRow.LayoutParams params2 = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT);
+		params2.setMargins(78, 9, 0, 29);
 		
+		garis.setText("=======================================");
+		garis.setGravity(Gravity.CENTER);
 		spin.setAdapter(adapter);
-		jenis.setText("Jenis : "+Integer.toString(count));
+		jenis.setText("Jenis : ");
 		//jenis.setLayoutParams(params);
 		keterangan.setText("Keterangan : ");
+		ket.setHint("Keterangan");
 		total.setText("Jumlah(kg) :");
-		
+		tot.setHint("total");
 		//setRunTimeMargin(jenis,45,0,97,27);
 		
 		row1.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
 		//ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)jenis.getLayoutParams();
 		//mlp.setMargins(37, 13, 17, 0);
 		
+		row0.addView(garis);
 		row1.addView(jenis,params);
 		row1.addView(spin);
+		row2.addView(keterangan,params2);
+		row2.addView(ket);
+		row3.addView(total,params2);
+		row3.addView(tot);
 		
+		frame.addView(row0);
 		frame.addView(row1);
+		frame.addView(row2);
+		frame.addView(row3);
 		
 		ll.addView(frame, line);
 		line++;
